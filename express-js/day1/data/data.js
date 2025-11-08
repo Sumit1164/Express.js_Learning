8f8cf85dc2d0573fc5336018fef34b8db5dd69f2
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-08 12:18:57 +0530

    Get Request (it is for fetching data from server) in express.js

---
+++ express-js/day1/data/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    name: "Sumit",
+    displayname: "Sumit Tripathi",
+  },
+  {
+    id: 2,
+    name: "Amit",
+    displayname: "Amit Tripathi",
+  },
+  {
+    id: 3,
+    name: "Kashish",
+    displayname: "Kashish Tripathi",
+  },
+  {
+    id: 4,
+    name: "KaSu",
+    displayname: "KASU Tripathi",
+  },
+  {
+    id: 5,
+    name: "Shubh",
+    displayname: "Shubh Tripathi",
+  }
+];
+
+export default data;
--- express-js/day1/index.js
+++ express-js/day1/index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import userData from "./data/data.js"
 
 const app = express();
 
 const PORT = 8080;
+
+
+// 1. Get Request (it is for fetching data from server)
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello world!")
+})
+
+// Industry Standards (for user data info)
+app.get("/api/v1/users", (req, res) => {
+    res.status(200).send(userData)
+})
+
+
+
 
 app.listen(PORT, (req, res)=> {
     console.log(`Server is running at ${PORT} PORT:`);
