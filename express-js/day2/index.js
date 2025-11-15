6bbfec3e0dd91edd7b20dab867f22e2ad4fa5eb2
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-15 22:33:22 +0530

    Day2, today learn middleware, and it's types

---
+++ express-js/day2/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+// const PORT = 8080
+
+// Global middleware
+function SayHiMiddleware(req, res, next) {
+  console.log("Hi i'm middleware✋")
+  next()
+}
+
+app.use(SayHiMiddleware)
+
+app.get("/", (req, res) => {
+  res.send('Hello world!')
+})
+
+app.get('/users', (req, res) => {
+  res.send("I'm user")
+})
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+})
+
+
+
+// 1. Global middleware
+// 2. Specific route middleware
+// 3. Inbuilt middleware
