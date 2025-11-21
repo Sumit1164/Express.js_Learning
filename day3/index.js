3cee1d0242cdd4591e3eae5a239347ee166287c1
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-21 20:20:24 +0530

    Cookies in express.js

---
+++ day3/index.js
@@ -0,0 +1,14 @@
+//! Cookies in express.js
+
+
+// import express from 'express'
+
+// const app = express();
+// app.get("/", (req, res) => {
+//     res.status(200).send("Hello world")
+// })
+// const PORT = 3000
+
+// app.listen(PORT, () => {
+//     console.log("Server running at https://localhost:3000", PORT)
+// })
