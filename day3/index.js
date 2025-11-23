1445fff500621f82c676cc82bf087308fad97946
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-23 23:39:30 +0530

    Cookies in express, very important for manage some info...

--- day3/index.js
+++ day3/index.js
@@ -1,14 +1,27 @@
 //! Cookies in express.js
 
 
-// import express from 'express'
+import express from 'express'
+const PORT = 8080;
 
-// const app = express();
-// app.get("/", (req, res) => {
-//     res.status(200).send("Hello world")
-// })
-// const PORT = 3000
+const app = express();
+app.get("/", (req, res) => {
+    res.cookie("Name", "express", {
+        maxAge:1000 *10  //60 * 60 *24 // cookies life take milisecond 1000 
+    })
+    res.send("Hello world✋💀")
+})
 
-// app.listen(PORT, () => {
-//     console.log("Server running at https://localhost:3000", PORT)
-// })
+app.get("/product", (req, res) => {
+    // console.log(req.cookies)     //! Show undefined
+    console.log(req.headers.cookie)
+    req.status(200).send({
+        id: 1,
+        name: "S7T-Shirt",
+        price:"$500"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:8080", PORT)
+})
