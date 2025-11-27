a9295e659512987f2268c077f65bbae6b71fcf10
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-27 13:10:22 +0530

    check user authorized by using cookies use middleware

--- day3/index.js
+++ day3/index.js
@@ -2,26 +2,41 @@
 
 
 import express from 'express'
+import cookieParser from 'cookie-parser';
+
 const PORT = 8080;
 
 const app = express();
+app.use(cookieParser("Secert"))
+
 app.get("/", (req, res) => {
-    res.cookie("Name", "express", {
-        maxAge:1000 *10  //60 * 60 *24 // cookies life take milisecond 1000 
-    })
-    res.send("Hello world✋💀")
-})
+    res.cookie("name", "express", {
+        maxAge: 1000 * 60 * 60 * 24, // cookies life take MeliSecond 1000 
+    });
+    res.send("Hello world✋💀");
+});
 
 app.get("/product", (req, res) => {
+    //? Various method to access cookies
     // console.log(req.cookies)     //! Show undefined
-    console.log(req.headers.cookie)
-    req.status(200).send({
-        id: 1,
-        name: "S7T-Shirt",
-        price:"$500"
-    })
-})
+    // console.log(req.headers.cookie)  //! name=express
+    // console.log("Cookies: ", req.cookies);  //! proper parser cookie
+
+
+    console.log("Cookies: ", req.cookies);
+
+    if (req.cookies.name && req.cookies.name === "express") {
+        res.status(200).send({
+            id: 1,
+            name: "S7T-Shirt",
+            price: "$500",
+        });
+    }
+
+    res.status(403).send("You are not authorized to view this page") 
+
+});
 
 app.listen(PORT, () => {
-    console.log("Server running at http://localhost:8080", PORT)
+    console.log("Server running at http://localhost:8080");
 })
