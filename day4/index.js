a72e1ce675caa0a6da360ed7e59aaf6aa9ec20c0
Sumit tripathi <sumittripathi8795@gmail.com>
2025-12-01 10:16:27 +0530

    session in express. use session to create an login and logout

--- day4/index.js
+++ day4/index.js
@@ -1,11 +1,41 @@
+import cookieParser from "cookie-parser";
 import express from "express"
-
+import session from "express-session";
 const app = express();
 
+app.use(session(
+    {
+        secret: "mysecret@",
+        saveUninitialized: false, // me nahi chahata ki koi user bina login kre mera app use kre isliye use krte h
+        resave: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24  // 1day avaliable
+        }
+    }
+))
+
+app.use(cookieParser("SUMITs7t"))
+
 app.get("/", (req, res) => {
+    console.log(req.session)
+    console.log(req.session);
     res.send("Hello World!")
 })
+app.get("/login", (req, res) => {
+    req.session.user = {
+        name: "Sumit Tripathi",
+        age: 21,
+        email: "sumit7@gmail.com"
+    }
+    res.send(`${req.session.user.name} is logged in`)
+})
 
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.send('Logged out')
+})
+
+//9:46
 
 app.listen(8080, () => {
     console.log("Server is running at port 8080");
