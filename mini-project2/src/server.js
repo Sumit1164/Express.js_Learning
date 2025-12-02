b545cd48841cfa34fb976f2cd141c54fff40b195
Sumit tripathi <sumittripathi8795@gmail.com>
2025-12-02 13:59:03 +0530

    add session

--- mini-project2/src/server.js
+++ mini-project2/src/server.js
@@ -4,6 +4,11 @@ import express from "express";
 import session from "express-session";
 
 
+
+import authRoute from "./routes/auth.routes.js"
+
+
+
 const app = express();
 const PORT = 8080;
 
@@ -12,12 +17,26 @@ const PORT = 8080;
 // Global Middleware
 app.use(express.json())
 
+app.use(session({
+    secret: "My-secret-key is I don't know",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge:100*60 *60 *24
+    }
+}))
 
 
 // Routes
 app.get("/", (req, res) => {
     res.send("Welcome to Task Manager API📔");
 })
+
+
+app.use("/auth", authRoute)
+
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on PORT ${PORT}`);
