f2e17845ea1966db59cd0cc16120e3e0a166537e
Sumit tripathi <sumittripathi8795@gmail.com>
2025-12-03 00:22:50 +0530

    task controller is basically handle user tasks

---
+++ mini-project2/src/controller/task.controller.js
@@ -0,0 +1,6 @@
+import {readTask, writeTask} from "../utils/file.utils.js"
+
+export const getAllTask = () => { }
+export const createTask=()=>{}
+export const updateTask=()=>{}
+export const deleteTask=()=>{}
