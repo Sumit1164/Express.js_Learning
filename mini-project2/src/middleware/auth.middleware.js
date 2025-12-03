c24bedf4cddc250a35764d276304571ad88d42e3
Sumit tripathi <sumittripathi8795@gmail.com>
2025-12-03 21:35:34 +0530

    something wrong

--- mini-project2/src/middleware/auth.middleware.js
+++
@@ -1,6 +0,0 @@
-export const authMiddleware = (req, res, next) => {
-    if (req.session && req.session.user) {
-        return next();
-    }
-    res.status(401).json({message:"Unauthorized: Please Login"})
-}
